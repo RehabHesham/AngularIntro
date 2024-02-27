8e24575127332b629cb4fa18649e728effa13fb4
RehabHesham <rehabhesham98@yahoo.com>
2024-02-27 12:10:14 +0200

    Day1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/navBar/navBar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { HomeComponent } from './components/home/home.component';
 
+// @FunctionName()    => decorators
 @NgModule({
   declarations: [
-    AppComponent
+    // register for components, directives,  and pipes declared by this module only
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    AsideComponent,
+    HomeComponent,
   ],
   imports: [
+    // register for related modules
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    // registration  of services used that can be injected anywhere in
+  ],
+  bootstrap: [AppComponent], // first component to run
 })
-export class AppModule { }
+export class AppModule {}
